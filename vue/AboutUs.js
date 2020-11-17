0e5fa57d9c6aedba3770c9a52a568039f932e8d9
Hrach <maxpyger@gmail.com>
2020-11-17 22:38:31 +0400

    aboutus page now loads own data in its component file ...

--- vue/AboutUs.js
+++ vue/AboutUs.js
@@ -3,7 +3,7 @@ import { default as Header } from './partials/Header.js';
 export default {
     template: `
 <header-vue pagetitle="About Us" :header="prop.header"></header-vue>
-<section class="container">
+<section class="container-fluid">
     {{ content }}
 </section>
     `,
@@ -17,5 +17,12 @@ export default {
         content() {
             return ( this.prop.api && this.prop.api.content )? this.prop.api.content: "Default text";
         }
+    },
+    beforeCreate() {
+        var vue = this;
+
+        $.post( baseUrl + '/api/aboutus', {}, function( data, status ) {
+            Object.assign( vue.prop, data )
+        }, 'json' );
     }
 }
